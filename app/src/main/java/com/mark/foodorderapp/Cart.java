2e160e4fee447d02dc385d782be3862b404791f5
mark <m4rkc93@gmail.com>
2018-03-10 23:44:15 +0000

    Implemented submitting the order with an address
    Order gets placed into firebase database

--- app/src/main/java/com/mark/foodorderapp/Cart.java
+++ app/src/main/java/com/mark/foodorderapp/Cart.java
@@ -1,14 +1,23 @@
 package com.mark.foodorderapp;
 
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mark.foodorderapp.Common.Common;
 import com.mark.foodorderapp.Model.Order;
+import com.mark.foodorderapp.Model.Request;
 import com.mark.foodorderapp.ViewHolder.CartAdapter;
 import com.mark.foodorderapp.database.Database;
 
@@ -25,7 +34,7 @@ public class Cart extends AppCompatActivity {
     RecyclerView.LayoutManager layoutmanager;
 
     FirebaseDatabase database;
-    DatabaseReference request;
+    DatabaseReference requests;
 
     TextView totalPrice;
     FButton btnPlace;
@@ -40,7 +49,7 @@ public class Cart extends AppCompatActivity {
         setContentView(R.layout.activity_cart);
 
         database = FirebaseDatabase.getInstance();
-        request=database.getReference("requests");
+        requests=database.getReference("requests");
 
         recyclerView = (RecyclerView)findViewById(R.id.listCart);
         recyclerView.setHasFixedSize(true);
@@ -49,8 +58,57 @@ public class Cart extends AppCompatActivity {
         
         totalPrice = (TextView)findViewById(R.id.total);
         btnPlace = (FButton)findViewById(R.id.orderBtn);
-        
+
+        btnPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog();
+            }
+        });
+
         loadOrder();
+    }
+
+    private void alertDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Cart.this);
+        alertDialog.setTitle("Now we need to know where you are!");
+        alertDialog.setMessage("Please enter your address: ");
+
+        final EditText edtAddress = new EditText (Cart.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        edtAddress.setLayoutParams(lp);
+        alertDialog.setView(edtAddress);
+        alertDialog.setIcon(R.drawable.cart);
+
+        alertDialog.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Request request = new Request(
+                        Common.currentUser.getPhone(),
+                        Common.currentUser.getName(),
+                        edtAddress.getText().toString(),
+                        totalPrice.getText().toString(),
+                        cart
+                );
+                requests.child(String.valueOf(System.currentTimeMillis())).setValue(request);
+
+                new Database(getBaseContext()).emptyCart();
+                Toast.makeText(Cart.this, "Order Confirmed", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
     }
 
     private void loadOrder()
